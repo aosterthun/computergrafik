35c72ad31cf805e2cdf65aa5b99ae3bec2db65f3
DaCortex <manuelhartmann@outlook.com>
2016-10-19 17:52:47 +0200

    Add constructor for reference planet

--- application/include/application_planet.hpp
+++ application/include/application_planet.hpp
@@ -1,6 +1,22 @@
 struct Planet
 {
+	Planet( float s, float r, float d):
+	size{s},
+	rotationSpeed{r},
+	distance{d},
+	reference_planet{nullptr}
+	{}
+
+	Planet( float s, float r, float d, Planet* rp):
+	size{s},
+	rotationSpeed{r},
+	distance{d},
+	reference_planet{rp}
+	{}
+
 	float size;
 	float rotationSpeed;
 	float distance;
+
+	Planet* reference_planet;
 };
