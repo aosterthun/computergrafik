1b5020a67e52286717415e8ac01b72888ab5ebaf
DaCortex <manuelhartmann@outlook.com>
2016-10-19 18:53:12 +0200

    Buggy version for maintenance

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -38,7 +38,18 @@ void ApplicationSolar::upload_planet_transforms(Planet const& planet) const{
     model_matrix = glm::rotate(model_matrix, float(glfwGetTime() * planet.rotationSpeed), turning_axis);
     //model_matrix = glm::rotate(glm::fmat4{}, float(glfwGetTime()), turning_axis);
 
-    model_matrix = glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, planet.distance});
+    //check wether the planet has a reference planet
+    if(planet.reference_planet == nullptr)
+    {
+      //normal translation
+      model_matrix = glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, planet.distance});
+    }
+    else 
+    {
+      Planet refPlan = *planet.reference_planet;
+      model_matrix = glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, refPlan.distance}) * glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, planet.distance });
+      //model_matrix = glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, planet.distance}) * glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, refPlan.distance });
+    }
 
     model_matrix = glm::scale(model_matrix, glm::fvec3{ planet.size, planet.size, planet.size});
 
@@ -59,17 +70,27 @@ void ApplicationSolar::upload_planet_transforms(Planet const& planet) const{
 }
 
 void ApplicationSolar::render() const {
+  
+  //Planet sun{ 1.0f, 0.0f, 0.0f};
+  Planet earth{ 0.14f,  0.9f, 7.0f};
+  Planet moon{ 0.03f, 0.3f, 0.3f, &earth};
 
-  std::vector<Planet> planets = { Planet{ 1.0f, 0.0f, 0.0f}, //sun
-                                  Planet{ 0.5f,  1.5f, 5.0f}, //mercury
-                                  Planet{ 0.25f,  1.3f, 6.6f}, //venus
-                                  Planet{ 0.14f,  0.9f, 7.0f}, //earth
-                                  Planet{ 0.4f, 1.0f, 9.0f}, //mars
-                                  Planet{ 0.43f, 0.9f, 12.0f}, //jupiter
-                                  Planet{ 0.22f, 0.6f, 14.0f}, //saturn
-                                  Planet{ 0.42f, 0.3f, 16.0f}, //uranus
-                                  Planet{ 0.15f, 0.4f, 19.0f} //neptune
-                                };
+  std::vector<Planet> planets;
+
+  /*planets = { Planet{ 1.0f, 0.0f, 0.0f},
+              Planet{ 0.5f,  1.5f, 5.0f}, //mercury
+              Planet{ 0.25f,  1.3f, 6.6f}, //venus
+              earth,
+              moon,
+              Planet{ 0.4f, 1.0f, 9.0f}, //mars
+              Planet{ 0.43f, 0.9f, 12.0f}, //jupiter
+              Planet{ 0.22f, 0.6f, 14.0f}, //saturn
+              Planet{ 0.42f, 0.3f, 16.0f}, //uranus
+              Planet{ 0.15f, 0.4f, 19.0f} //neptune
+            };
+            */
+  planets = {earth, moon}; // testing
+
 
   for (std::vector<Planet>::iterator i = planets.begin(); i != planets.end(); ++i)
   {
