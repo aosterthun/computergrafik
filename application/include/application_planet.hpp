55358c16239ad452f59434a8a96e160407701816
DaCortex <manuelhartmann@outlook.com>
2016-10-25 15:43:53 +0200

    Made Planet a class with model_index method

--- application/include/application_planet.hpp
+++ application/include/application_planet.hpp
@@ -1,7 +1,21 @@
-#include <memory>
+#ifndef APPLICATION_PLANET_HPP
+#define APPLICATION_PLANET_HPP
 
-struct Planet
+#include <memory>
+#include <glbinding/gl/gl.h>
+
+//dont load gl bindings from glfw
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+
+class Planet
 {
+public:
 	Planet( std::string n, float s, float r, float d):
 	name{n},
 	size{s},
@@ -18,6 +32,27 @@ struct Planet
 	reference_planet{rp}
 	{}
 
+	glm::fmat4 model_matrix() const
+	{
+		glm::fmat4 tmp{};
+
+		if(reference_planet != nullptr)
+		{
+			tmp *= reference_planet->model_matrix();
+		}
+
+    	glm::fvec3 turning_axis{0.0f, 1.0f, 0.0f};
+
+
+    	tmp *= glm::rotate(glm::fmat4{}, float(glfwGetTime() * this->rotationSpeed), turning_axis);
+    	tmp *= glm::translate(glm::fmat4{}, glm::fvec3{0.0f, 0.0f, this->distance});
+    	
+    	//not needed
+    	//tmp = glm::scale(model_matrix, glm::fvec3{ this->size, this->size, this->size});
+
+		return tmp;
+	}
+
 	std::string name;
 
 	float size;
@@ -26,3 +61,5 @@ struct Planet
 
 	std::shared_ptr<Planet> reference_planet;
 };
+
+#endif
