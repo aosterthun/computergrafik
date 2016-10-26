a511f22f978759cf05a9e4a3b70bd9d0ee715c15
DaCortex <manuelhartmann@outlook.com>
2016-10-26 13:17:54 +0200

    Renamed planet.hpp

---
+++ application/include/planet.hpp
@@ -0,0 +1,75 @@
+#ifndef APPLICATION_PLANET_HPP
+#define APPLICATION_PLANET_HPP
+
+#include <memory>
+#include <glbinding/gl/gl.h>
+
+//dont load gl bindings from glfw
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+
+struct Planet
+{
+  //CONSTRUCTORS
+  Planet( std::string n, float s, float r, float d):
+  name{n},
+  size{s},
+  rotationSpeed{r},
+  turningAxis{0.0f, 1.0f, 0.0f},
+  distance{d},
+  reference_planet{nullptr}
+  {}
+
+  Planet( std::string n, float s, float r, glm::fvec3 a, float d):
+  name{n},
+  size{s},
+  rotationSpeed{r},
+  turningAxis{a},
+  distance{d},
+  reference_planet{nullptr}
+  {}
+
+  Planet( std::string n, float s, float r, float d, std::shared_ptr<Planet> rp):
+  name{n},
+  size{s},
+  rotationSpeed{r},
+  turningAxis{0.0f, 1.0f, 0.0f},
+  distance{d},
+  reference_planet{rp}
+  {}
+
+  //MEMBERS
+  std::string name;
+
+  float size;
+  float rotationSpeed;
+
+  glm::fvec3 turningAxis;
+
+  float distance;
+  std::shared_ptr<Planet> reference_planet;
+};
+
+glm::fmat4 model_matrix(std::shared_ptr<Planet> const& planet)
+{
+  glm::fmat4 tmp{};
+
+  if(planet->reference_planet != nullptr) {
+    tmp *= model_matrix(planet->reference_planet);
+  }
+
+  tmp *= glm::rotate(glm::fmat4{}, float(glfwGetTime() * planet->rotationSpeed), planet->turningAxis);
+  tmp *= glm::translate(glm::fmat4{}, glm::fvec3{0.0f, 0.0f, planet->distance});
+  
+  //not needed
+  //tmp = glm::scale(model_matrix, glm::fvec3{ this->size, this->size, this->size});
+
+  return tmp;
+}
+
+#endif
