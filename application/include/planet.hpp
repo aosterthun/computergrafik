27cb062b364620ba324bfa779ea3e50fdfcc6a75
DaCortex <manuelhartmann@outlook.com>
2016-11-21 17:18:02 +0100

    Add texture path to constructor

--- application/include/planet.hpp
+++ application/include/planet.hpp
@@ -22,7 +22,7 @@ struct Planet
           float s,
           float r,
           float d,
-          Material const& m,
+          Material const& m,  //planet having a material
           int t,
           std::shared_ptr<Planet> rp = nullptr):
   name{n},
@@ -30,6 +30,25 @@ struct Planet
   rotationSpeed{r},
   distance{d},
   material{m},
+  texture_path{"NONE"},
+  turningAxis{0.0f, 1.0f, 0.0f},
+  planetType{t},
+  reference_planet{rp}
+  {}
+
+  Planet( std::string n,
+          float s,
+          float r,
+          float d,
+          std::string const& tp,  //planet having a texture
+          int t,
+          std::shared_ptr<Planet> rp = nullptr):
+  name{n},
+  size{s},
+  rotationSpeed{r},
+  distance{d},
+  material{{1.0f, 1.0f, 1.0f}},
+  texture_path{tp},
   turningAxis{0.0f, 1.0f, 0.0f},
   planetType{t},
   reference_planet{rp}
@@ -46,6 +65,7 @@ struct Planet
   rotationSpeed{r},
   distance{d},
   material{{1.0f, 1.0f, 1.0f}},
+  texture_path{"NONE"},
   turningAxis{0.0f, 1.0f, 0.0f},
   planetType{t},
   reference_planet{rp}
@@ -65,6 +85,9 @@ struct Planet
 
   //Material
   Material material;
+
+  //Texture path
+  std::string texture_path;
 
   //Shader
   int planetType;
